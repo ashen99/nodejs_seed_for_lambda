168b0621f5e1243b1b1c912a67fa9d2fd1be773a
Lochana Thiwanka <lochanathiwanka62@gmail.com>
2021-12-22 16:37:20 +0530

    added mockSbuRepository

---
+++ apps/sbu-app/src/sbu/sbu.service.spec.ts
@@ -0,0 +1,29 @@
+import { Test, TestingModule } from '@nestjs/testing';
+import { SbuService } from './sbu.service';
+import { getRepositoryToken } from '@nestjs/typeorm';
+import { Sbu } from './entities/sbu.entity';
+
+describe('SbuService', () => {
+  let service: SbuService;
+
+  const mockSbuRepository = {};
+
+  beforeEach(async () => {
+    const module: TestingModule = await Test.createTestingModule({
+      providers: [
+        SbuService,
+        SbuService,
+        {
+          provide: getRepositoryToken(Sbu),
+          useValue: mockSbuRepository,
+        },
+      ],
+    }).compile();
+
+    service = module.get<SbuService>(SbuService);
+  });
+
+  it('should be defined', () => {
+    expect(service).toBeDefined();
+  });
+});
