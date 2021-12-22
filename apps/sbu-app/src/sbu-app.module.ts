995253e49830de38b32349b35c2d60abde8c2021
Lochana Thiwanka <lochanathiwanka62@gmail.com>
2021-12-22 16:38:21 +0530

    added GraphQL & TypeOrm modules

--- apps/sbu-app/src/sbu-app.module.ts
+++ apps/sbu-app/src/sbu-app.module.ts
@@ -1,9 +1,28 @@
 import { Module } from '@nestjs/common';
 import { SbuAppController } from './sbu-app.controller';
 import { SbuAppService } from './sbu-app.service';
+import { SbuModule } from './sbu/sbu.module';
+import { GraphQLModule } from '@nestjs/graphql';
+import { join } from 'path';
+import { Sbu } from './sbu/entities/sbu.entity';
+import { TypeOrmModule } from '@nestjs/typeorm';
 
 @Module({
-  imports: [],
+  imports: [
+    SbuModule,
+    GraphQLModule.forRoot({
+      autoSchemaFile: join(process.cwd(), 'src/sbu-schema.gql'),
+    }),
+    TypeOrmModule.forRoot({
+      type: 'postgres',
+      host: 'localhost',
+      username: 'postgres',
+      password: '1234',
+      database: 'sbu',
+      entities: [Sbu],
+      synchronize: true,
+    }),
+  ],
   controllers: [SbuAppController],
   providers: [SbuAppService],
 })
