f0f09b600993f1b34165a8fa4507b3854b56f385
Lochana Thiwanka <lochanathiwanka62@gmail.com>
2021-12-22 23:31:53 +0530

    added GraphQL & TypeOrm modules

--- apps/user-permission-app/src/user-permission-app.module.ts
+++ apps/user-permission-app/src/user-permission-app.module.ts
@@ -2,9 +2,27 @@ import { Module } from '@nestjs/common';
 import { UserPermissionAppController } from './user-permission-app.controller';
 import { UserPermissionAppService } from './user-permission-app.service';
 import { PermissionModule } from './permission/permission.module';
+import { GraphQLModule } from '@nestjs/graphql';
+import { join } from 'path';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { Permission } from './permission/entities/permission.entity';
 
 @Module({
-  imports: [PermissionModule],
+  imports: [
+    PermissionModule,
+    GraphQLModule.forRoot({
+      autoSchemaFile: join(process.cwd(), 'src/permission-schema.gql'),
+    }),
+    TypeOrmModule.forRoot({
+      type: 'postgres',
+      host: 'localhost',
+      username: 'postgres',
+      password: '1234',
+      database: 'permission',
+      entities: [Permission],
+      synchronize: true,
+    }),
+  ],
   controllers: [UserPermissionAppController],
   providers: [UserPermissionAppService],
 })
