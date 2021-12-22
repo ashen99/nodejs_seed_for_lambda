264076c5ed76ac06435dffa5561c0ddef7d2e48d
pamuditha-fcode <pamuditha@fcodelabs.com>
2021-12-22 23:07:56 +0530

    update more tests for plant service

--- apps/plant-app/src/plant/plant.service.spec.ts
+++ apps/plant-app/src/plant/plant.service.spec.ts
@@ -1,18 +1,62 @@
 import { Test, TestingModule } from '@nestjs/testing';
 import { PlantService } from './plant.service';
+import { Plant } from './entities/plant.entity';
+import { Repository } from 'typeorm';
+import { getRepositoryToken } from '@nestjs/typeorm';
+
+const plantArray = [
+  new Plant('Colombo'),
+  new Plant('Kadawatha'),
+  new Plant('Kandy'),
+];
+
+const onePlant = new Plant('Ja Ela');
 
 describe('PlantService', () => {
   let service: PlantService;
+  let repo: Repository<Plant>;
 
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
-      providers: [PlantService],
+      providers: [
+        {
+          provide: getRepositoryToken(Plant),
+          useValue: {
+            find: jest.fn().mockResolvedValue(plantArray),
+            findOneOrFail: jest.fn().mockReturnValue(onePlant),
+            create: jest.fn().mockReturnValue(onePlant),
+            update: jest.fn().mockReturnValue(onePlant),
+            delete: jest.fn().mockResolvedValue(true),
+          },
+        },
+        PlantService,
+      ],
     }).compile();
 
     service = module.get<PlantService>(PlantService);
+    repo = module.get<Repository<Plant>>(getRepositoryToken(Plant));
   });
 
   it('should be defined', () => {
     expect(service).toBeDefined();
   });
+
+  describe('findAll', () => {
+    it('Should return an array of plants', async () => {
+      const plants = await service.findAll();
+      expect(plants).toEqual(plantArray);
+    });
+  });
+
+/*  describe('findOne', () => {
+    it('Should return single Plant', async () => {
+      const repoSpy = jest.spyOn(repo, 'findOneOrFail');
+      await expect(
+        service.findOne('5dbe839b-b803-4aab-9943-6cb5aa361869'),
+      ).resolves.toEqual(onePlant);
+      expect(repoSpy).toBeCalledWith({
+        id: '5dbe839b-b803-4aab-9943-6cb5aa361869',
+      });
+    });
+  });*/
 });
