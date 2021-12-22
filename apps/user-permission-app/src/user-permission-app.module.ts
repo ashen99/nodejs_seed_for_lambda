7d29bada0f9c18c749e4d5a5094a24e8a00fd45b
Lochana Thiwanka <lochanathiwanka62@gmail.com>
2021-12-22 22:22:10 +0530

    added permission module

--- apps/user-permission-app/src/user-permission-app.module.ts
+++ apps/user-permission-app/src/user-permission-app.module.ts
@@ -1,9 +1,10 @@
 import { Module } from '@nestjs/common';
 import { UserPermissionAppController } from './user-permission-app.controller';
 import { UserPermissionAppService } from './user-permission-app.service';
+import { PermissionModule } from './permission/permission.module';
 
 @Module({
-  imports: [],
+  imports: [PermissionModule],
   controllers: [UserPermissionAppController],
   providers: [UserPermissionAppService],
 })
